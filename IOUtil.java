531ba9aea98b4a754f94f3674dd1709a563b5cbd
thisMagpie <thismagpie@live.com>
2013-07-06 23:41:16 +0100

    Tidy up
    Remove white spaces etc

--- IOUtil.java
+++ IOUtil.java
@@ -1,23 +1,20 @@
-/*
-    IOUtil.java  
-    ============
-    
-    Assistant methods for handling IO related happenings
-
-    Copyright (C) 2013  Magdalen Berns
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+* IOUtil.java  
+* ============
+* Copyright (C) 2013  Magdalen Berns
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* This file provides a library of static methods for handling common 
+* IO requirements.
 */
 
 import java.util.Scanner;
@@ -25,6 +22,10 @@ import java.io.*;
 
 public class IOUtil{
 
+    /*
+    * does not accept anything but doubles 
+    * @ return the first double
+    */
     public static double skipToDouble(Scanner scanner){
 
         while (scanner.hasNext() && !scanner.hasNextDouble()){
@@ -32,16 +33,20 @@ public class IOUtil{
         }
         return scanner.hasNextDouble() ? scanner.nextDouble() : Double.NaN;
     }
-    public static FileReader file(String fileName)throws FileNotFoundException {
+
+    public static FileReader file(String fileName) throws FileNotFoundException {
         return new FileReader(fileName);
     }
     //read the user input
     public static String typedInput() throws IOException {
         BufferedReader keyIn = new BufferedReader(new InputStreamReader(System.in));
         return keyIn.readLine();
-
     }
-    //Picks out the first integer value it sees and returns it.
+
+    /**
+    * does not accept anything but integers
+    * @ return the first integer value
+    */
     public static int skipToInt(Scanner scanner) {
 
         while (scanner.hasNext() && !scanner.hasNextInt()) {
@@ -49,16 +54,24 @@ public class IOUtil{
         }
         return scanner.hasNextInt() ? scanner.nextInt() : (int) Double.NaN;//workaround
     }
-    // Call this every time wrong thing is typed.
+
+    /**
+    * Call this every time wrong thing is typed.
+    */
     public static void abuse() {
         System.out.println("Invalid entry.");
         System.out.println("Abusive statement.");
         System.exit(0);
     }
-    public static String getFileName() throws IOException {
-		System.out.printf("Type file name or hit '!' \n"); 
-	 	String typed= typedInput();
-		System.out.printf("Found %s \n", typed); 
-		return typed;
-			}
+
+    public static String fileName() throws IOException {
+        System.out.printf("Type file name or hit '!' \n"); 
+        String typed= typedInput();
+        System.out.printf("Found %s \n", typed); 
+        return typed;
+    }
+
+    public static String enterValue(String kindOf) {
+        return "Enter " + kindOf + "value:";
+    }
 }
