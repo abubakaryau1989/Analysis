1c6d723a83e8ca2d7f4fd6c9e9d7d6ffa68026fb
thisMagpie <thismagpie@live.com>
2014-01-25 10:28:35 +0000

    tidy format

--- fin.sh
+++ fin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 gnuplot << TOEND
-# Set the output file
 
+# Set the output file type
 set terminal png 
 set output 'z.png' 
 unset key
@@ -19,14 +19,11 @@ fit u(x) "linear.txt" using 1:2 via m,b
 
 set xzeroaxis linestyle 2 lt 2 lc 9
 set xlabel "Applied Current mA" offset -0.3 -4.0
-
 set ylabel "Voltage mV"  
-
 set xrange [ -15.00 : 15.00 ]
 set yrange [ -3.0 : 3.0 ]
 
 plot  u(x), 'z.png' using 1:2:4:5 with xyerrorbars
-	
 TOEND
 
 
