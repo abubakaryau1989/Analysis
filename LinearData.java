b05f524648daa434e581e08b95c36341dad1d6a6
thisMagpie <thismagpie@live.com>
2014-01-25 08:25:37 +0000

    fix yet more horror

--- LinearData.java
+++ LinearData.java
@@ -28,21 +28,20 @@ import java.io.FileNotFoundException;
 public class LinearData{
 
     public static void main(String[] args) throws IOException {
-    String fileName = IOUtil.getFileName();
-    Scanner scan = new Scanner(new BufferedReader(new FileReader("files/" + fileName)));
-    PrintWriter fitFout = new PrintWriter("files/linear.txt");
-    int length = IOUtil.skipToInt(scan);
-    double xError=IOUtil.skipToDouble(scan);
-    double yError= IOUtil.skipToDouble(scan);
-    double[][] data = PlotReader.data2Column(scan, length); 
-    PlotUtil p = new PlotUtil(data);
-    
-    PrintWriter residualsWriter=new PrintWriter("files/residuals.txt");
-    
-     handleData(fitFout,residualsWriter,p.x(),p.y(),xError, yError);
-  }
+        String fileName = IOUtil.getFileName();
+        Scanner scan = new Scanner(new BufferedReader(new FileReader("files/" + fileName)));
+        PrintWriter fitFout = new PrintWriter("files/linear.txt");
+        int length = IOUtil.skipToInt(scan);
+        double xError=IOUtil.skipToDouble(scan);
+        double yError= IOUtil.skipToDouble(scan);
+        double[][] data = PlotReader.data2Column(scan, length); 
+        PlotUtil p = new PlotUtil(data);
+        
+        PrintWriter residualsWriter=new PrintWriter("files/residuals.txt");
+        handleData(fitFout,residualsWriter, p.x(), p.y(), xError, yError);
+    }
 
-  public static void handleData(PrintWriter fitFout,
+    public static void handleData(PrintWriter fitFout,
                                 PrintWriter residualsWriter,
                                 double[] x,
                                 double[] y,
@@ -50,58 +49,58 @@ public class LinearData{
                                 double yError
                                 ){
     
-    double xMean= StatsUtil.mean(x);
-    double yMean= StatsUtil.mean(y);
+        double xMean= StatsUtil.mean(x);
+        double yMean= StatsUtil.mean(y);
+        
+        double xVar= StatsUtil.variance(xMean,x);
+        double yVar= StatsUtil.variance(yMean,y);
+
+        double gradient = StatsUtil.gradient(StatsUtil.covariance(xMean,
+                                                                  yMean,
+                                                                  x,
+                                                                  y),
+                                            xVar);
+
+        double offset = StatsUtil.yIntercept(xMean,yMean, gradient);
+        double[] fit = StatsUtil.fit(x, gradient, offset);
+        PlotWriter.errors(x,
+                          StatsUtil.residuals(y,fit),
+                          xError,
+                          Calculate.multiply(y,yError),
+                      residualsWriter);
+        residualsWriter.close(); 
+
+    	System.out.printf("\nLength of data = %2.0f  ",(float) x.length);
     
-    double xVar= StatsUtil.variance(xMean,x);
-    double yVar= StatsUtil.variance(yMean,y);
-
-    double gradient = StatsUtil.gradient(StatsUtil.covariance(xMean,
-                                                              yMean,
-                                                              x,
-                                                              y),
-                                         xVar);
-
-    double offset = StatsUtil.yIntercept(xMean,yMean, gradient);
-    double[] fit = StatsUtil.fit(x, gradient, offset);
-	PlotWriter.errors(x,
-	                  StatsUtil.residuals(y,fit),
-	                  xError,
-	                  Calculate.multiply(y,yError),
-	                  residualsWriter);
-	residualsWriter.close(); 
-
-	System.out.printf("\nLength of data = %2.0f  ",(float) x.length);
-
-    double standardError = StatsUtil.standardError(y, fit);
-	System.out.printf("\nGradient= %2.4f with error +/-  %2.4f ",
-	                  gradient,
-	                  StatsUtil.errorGradient(xVar,standardError, x.length)
-	                 );
-
-	System.out.printf("\nResidual sum squares  = %2.2f ",
-	                  Math.sqrt(standardError / (x.length-1)
-	                  );
-
-	System.out.printf("\nOffset = %g with error  +/-  %g ",
-	                  offset,
-	                  StatsUtil.errorOffset(x.length,
-	                                        xVar,
-	                                        xMean,
-	                                        standardError)
-                     );
-
-	System.out.printf("\nLinear Correlation Coefficient %g",
-    StatsUtil.linearCorrelationCoefficient(StatsUtil.regressionSumOfSquares(fit, yMean), yVar));
-
-	PlotWriter.errorsFit(x,
-	                     y,
-	                     fit,
-	                     xError,
-	                     Calculate.multiply(y, yError),
+        double standardError = StatsUtil.standardError(y, fit);
+    	System.out.printf("\nGradient= %2.4f with error +/-  %2.4f ",
+    	                  gradient,
+    	                  StatsUtil.errorGradient(xVar,standardError, x.length)
+    	                 );
+    
+    	System.out.printf("\nResidual sum squares  = %2.2f ",
+    	                  Math.sqrt(standardError / (x.length-1)
+    	                  );
+    
+    	System.out.printf("\nOffset = %g with error  +/-  %g ",
+    	                  offset,
+    	                  StatsUtil.errorOffset(x.length,
+    	                                        xVar,
+    	                                        xMean,
+    	                                        standardError)
+                         );
+    
+        System.out.printf("\nLinear Correlation Coefficient %g",
+        StatsUtil.linearCorrelationCoefficient(StatsUtil.regressionSumOfSquares(fit, yMean), yVar));
+        
+        PlotWriter.errorsFit(x,
+                         y,
+                         fit,
+                         xError,
+                         Calculate.multiply(y, yError),
                      fitFout);
-	fitFout.close();
-
-   	System.exit(0);
-  	}
+        fitFout.close();
+        
+        System.exit(0);
+    }
 }
