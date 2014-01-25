b1321442be6184858a03ca1aa17b785eeb9f9999
thisMagpie <thismagpie@live.com>
2014-01-25 10:40:44 +0000

    fix indentation horror

--- PlotReader.java
+++ PlotReader.java
@@ -24,42 +24,37 @@ import java.util.Scanner;
 public class PlotReader{
 
     public static double[] data1Column(Scanner scan, int length){
-    			double[] data=new double[length];
-        for (int i=0;i<data.length;i++){
-            data[i] = IOUtil.skipToDouble(scan);
-        }
+        double[] data=new double[length];
+        for (int i=0;i<data.length;i++) data[i] = IOUtil.skipToDouble(scan);
         return data;
     }
 
     //Initialise the array with the values from a file
     public static double[][] data2Column(Scanner scan, int length){
-    			double[][] data = new double[length][2];
+        double[][] data = new double[length][2];
         for (int i=0;i<data.length;i++){
             for (int j=0;j<data[0].length;j++){
                 data[i][j] = (float) IOUtil.skipToDouble(scan);
             }
         }
         return data;
-	}
+    }
 
     //Initialise the array with the values from a file
     public static double[][] data3Column(Scanner scan, int length){
-    			double[][] data = new double[length][3];
+        double[][] data = new double[length][3];
         for (int i=0;i<data.length;i++){
-            for (int j=0;j<data[0].length;j++){
-                data[i][j] = (float) IOUtil.skipToDouble(scan);
-            }
+            for (int j=0;j<data[0].length;j++)data[i][j] = (float) IOUtil.skipToDouble(scan);
         }
         return data;
-	}
+    }
+
     //Initialise the array with the values from a file
     public static double[][] data4Column(Scanner scan, int length){
-    			double[][] data = new double[length][4];
+        double[][] data = new double[length][4];
         for (int i=0;i<data.length;i++){
-            for (int j=0;j<data[0].length;j++){
-                data[i][j] = (float) IOUtil.skipToDouble(scan);
-            }
+            for (int j=0;j<data[0].length;j++) data[i][j] = (float) IOUtil.skipToDouble(scan);
         }
         return data;
-	}
+    }
 }
