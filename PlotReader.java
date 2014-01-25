6b8d9dcc3832d86fd1f5e63caa8db0f3ad5ac288
thisMagpie <thismagpie@live.com>
2014-01-25 10:37:35 +0000

    fix indentation horror

--- PlotReader.java
+++ PlotReader.java
@@ -1,22 +1,21 @@
-/*
-    PlotUtil.java
-    =============
-    
-    This is a utility for Reading files of data from plots 
-    Copyright (C) 2013  Magdalen Berns
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *   PlotUtil.java
+ *  ==============
+ *
+ *  Copyright (C) 2013-2014  Magdalen Berns <m.berns@sms.ed.ac.uk>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import java.util.Scanner;
